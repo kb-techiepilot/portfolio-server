1329071c050153d81d2beb07d6dac8f1ed8ab08f
balaji <balaji@localhost>
2021-05-21 13:40:32 +0530

    clearup

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    latestStockPrice: {
-        apiKey  : "4adef21fb3msh473ed3d95fbc66ap1b8a9djsn87c4e5827645",
-        apiHost : "latest-stock-price.p.rapidapi.com"
-    }
-  };
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.send("Welcome to backend server");
-});
-
-module.exports = router;
