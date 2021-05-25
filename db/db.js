fbf1a2ce3c28242c02812cfcf314e9772c701366
balaji <balaji@localhost>
2021-05-26 00:30:10 +0530

    moved to heroku

--- db/db.js
+++ db/db.js
@@ -9,23 +9,24 @@ const Pool = require("pg").Pool;
 // });
 
 //heroku configuration
-// const pool = new Pool({
-//     user: "ecdklsjcttltdj",
-//     password: "e16ee3a923992919539ebefb45eca306e954957ce803f05dba64b1691d6c53a7",
-//     database: "d8lnlcpqhio1si",
-//     host: "ec2-54-152-185-191.compute-1.amazonaws.com",
-//     port: "5432",
-//     ssl: { rejectUnauthorized: false }
-// });
-
 const pool = new Pool({
-    user: "portfolio@kb-shares",
-    password: "Root@123",
-    database: "postgres",
-    host: "kb-shares.postgres.database.azure.com",
+    user: "ecdklsjcttltdj",
+    password: "e16ee3a923992919539ebefb45eca306e954957ce803f05dba64b1691d6c53a7",
+    database: "d8lnlcpqhio1si",
+    host: "ec2-54-152-185-191.compute-1.amazonaws.com",
     port: "5432",
-    ssl: true
+    ssl: { rejectUnauthorized: false }
 });
+
+//azure psql configs
+// const pool = new Pool({
+//     user: "portfolio@kb-shares",
+//     password: "Root@123",
+//     database: "postgres",
+//     host: "kb-shares.postgres.database.azure.com",
+//     port: "5432",
+//     ssl: true
+// });
 
 module.exports = pool;
 
