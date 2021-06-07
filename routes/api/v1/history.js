f36b4f6af9eaad23c32cda13780c80c67f4db6d8
balaji <balaji@localhost>
2021-06-07 22:50:31 +0530

    Update history.js

--- routes/api/v1/history.js
+++ routes/api/v1/history.js
@@ -6,9 +6,7 @@ const nseIndia = new NseIndia()
 
 async function getHistoryData(symbol, startDate, endDate) {
     console.log(symbol);
-    // let history = await nseIndia.getEquityHistoricalData("SBIN", {start:"2021-06-02", end:"2021-06-04"});
     let history = await nseIndia.getEquityHistoricalData(symbol, {start:startDate, end:endDate});
-    // let history = await nseIndia.getEquityDetails("SBIN");
     return history;
 }
 function GetSortOrder() {    
@@ -24,14 +22,11 @@ function GetSortOrder() {
 router.get('/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
     const {start, end } = req.query;
-    console.log(symbol + " " + start + " " + end);
     const history = await getHistoryData(symbol, start, end);
     var data = {};
     var historyData = [];
-    console.log("legnth : " + history.length);
     history.forEach(ele => {
         ele.data.forEach(element => {
-            console.log(JSON.stringify(element) + "\n");
             data = element;
             historyData.push(data);
             data = {};
