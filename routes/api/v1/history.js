75e4d6056083476351f9ca49ce115f84233cab24
balaji <balaji@localhost>
2021-06-07 20:16:45 +0530

    Update history.js

--- routes/api/v1/history.js
+++ routes/api/v1/history.js
@@ -11,13 +11,34 @@ async function getHistoryData(symbol, startDate, endDate) {
     // let history = await nseIndia.getEquityDetails("SBIN");
     return history;
 }
+function GetSortOrder() {    
+    return function(a, b) {    
+        if (a.CH_TIMESTAMP > b.CH_TIMESTAMP) {    
+            return 1;    
+        } else {    
+            return -1;    
+        }     
+    }    
+}
 
 router.get('/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
     const {start, end } = req.query;
     console.log(symbol + " " + start + " " + end);
     const history = await getHistoryData(symbol, start, end);
-    res.json(history);
+    var data = {};
+    var historyData = [];
+    console.log("legnth : " + history.length);
+    history.forEach(ele => {
+        ele.data.forEach(element => {
+            console.log(JSON.stringify(element) + "\n");
+            data = element;
+            historyData.push(data);
+            data = {};
+        });
+    });
+    historyData = historyData.sort(GetSortOrder());
+    res.json(historyData);
 });
 
 module.exports = router;
