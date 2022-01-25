46c6b6d515a97a55846e0c02d9fe042150488295
balaji <balaji@localhost>
2022-01-25 22:57:29 +0530

    Update tvalert.js

--- routes/api/v2/tvalert.js
+++ routes/api/v2/tvalert.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const axios = require('axios');
 
 router.post('/', async (req, res) => {
-    console.log(req.body);
+    console.log(req);
     res.json(req.body);
-    const gainers = await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id=-1001793121016&text='+JSON.stringify(req.body));
+    const gainers = await axios.post('https://api.telegram.org/bot5007955067:AAGCvnc1AhD0Y11ukuM4zuuCKTb3obJVlwM/sendMessage?chat_id=-1001793121016&text='+JSON.stringify(req));
 });
 
 module.exports = router;
